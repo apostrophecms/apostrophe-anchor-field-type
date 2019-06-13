1e900c1a783c5911b586d147eef238a208ae5ecd
Stuart Romanek <stuart@punkave.com>
2019-06-13 10:26:56 -0400

    finalish

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const isUrl = require('validator/lib/isUrl')
+
 module.exports = {
   improve: 'apostrophe-schemas',
   afterConstruct: function (self) {
     self.addFieldAnchorType();
     self.pushAsset('script', 'idField', { when: 'user' });
+    self.addRoutes();
   },
 
   beforeConstruct: function(self, options) {},
 
   construct: function (self, options) {
     require('./lib/schemaField.js')(self, options);
+    self.httpify = function(s) {
+      if (!/^(f|ht)tps?:\/\//i.test(s)) {
+        s = "http://" + s;
+      }
+      return s;
+    },
+    self.addRoutes = function() {
+      self.route('post', 'get-choices', async function (req, res) {
+        try {
+          if (!req.body.key) {
+            return res.json({ error: true, msg: 'No key supplied' })
+          }
+          var key = req.body.key;
+          var type = req.body.fieldType;
+          var baseUrl = '';
+          var page;
+
+          if (type === 'join') {
+              baseUrl = req.baseUrlWithPrefix;
+              var criteria = { _id: key }
+              var doc = await self.apos.pages.find(req, criteria, {_url: 1}).toObject();
+              page = await axios.get(req.baseUrlWithPrefix + doc._url);
+          } else {
+            // if invalid url, bail
+            if (!isUrl(key)) {
+              return res.json([]);
+            }
+            page = await axios.get(self.httpify(key));
+          }
+
+          var $ = cheerio.load(page.data)
+          var choices = [];
+          $('[id]').each(function() {
+            choices.push({
+              label: $(this).attr('id'),
+              value: $(this).attr('id')
+            })
+          })
+          return res.json(choices);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    }
   }
 };
--- public/js/idField.js
+++ public/js/idField.js
@@ -4,29 +4,136 @@ apos.define('apostrophe-schemas', {
     self.addFieldType({
       name: 'id',
       populate: function (data, name, $field, $el, field, callback) {
-        var selectOptions = [];
-        var $elementsWithIds = $('[id]');
 
-        $elementsWithIds.each(function() {
-          var $this = $(this);
-          selectOptions.push({
-            label: $this.attr('id'),
-            value: $this.attr('id')
-          })
-        });
+        if (!field.remoteIdsFields) {
+          console.warn('This field requires a joinByOne field to watch');
+          console.warn('Set the property `remoteIdsField` to the field name');
+          return setImmediate(callback);
+        }
+
+        var $form = $field.parents('[data-apos-form]');
+        var observerConfig = { attributes: false, childList: true, subtree: false };
+        var remotes = {};
+        var observer;
+
+        if (_.isString(field.remoteIdsFields)) {
+          setupOptions([field.remoteIdsFields]);
+        } else {
+          setupOptions(field.remoteIdsFields);
+        }
+
+        setupObservers(remotes)
+
         
-        $field.find('option').remove();
+        function setupOptions(array) {
+          array.forEach(function(item) {
+            // shorthand, fieldname string
+            if (_.isString(item)) {
+              remotes[item] = {
+                fieldName: item
+              };
+              if (item[0] === '_') {
+                remotes[item].fieldType = 'join';
+                remotes[item].urlType = 'relative';
+              } else {
+                remotes[item].fieldType = 'external';
+                remotes[item].urlType = 'absolute';
+              }
+            } else {
+              // longform options
+              if (item.fieldName) {
+                remotes[item.fieldName] = {
+                  ...item
+                };
+              }
+            }
+          });
+        }
 
-        _.each(selectOptions, function (choice) {
-          var $option = $('<option></option>');
-          $option.text(choice.label);
-          $option.attr('value', choice.value);
-          $field.append($option);
-        });
+        function setupObservers(remotes) {
+          observer = new MutationObserver(function(mutationsList, observer) {
+            return getChoices(mutationsList, observer);
+          });
+          for (var remoteName in remotes) {
+            var $remoteField = $form.find('[data-name="'+ remoteName +'"]');
+            var $observable;
+            if (remotes[remoteName].fieldType === 'join') {
+              $observable = $remoteField.find('[data-choices]');
+            } else {
+              $observable = $remoteField.find(':input');
+              $observable.on('keyup', function() {
+                getChoices([ { target: $observable.get(0) } ]);
+              });
+            }
+            remotes[remoteName].$observable = $observable;
+            observer.observe($observable.get(0), observerConfig);
+          }
+        }
 
-        var value = ((data[name] === undefined) && field.choices[0]) ? field.choices[0].value : data[name];
+        function getChoices(mutationsList) {
+          var current;
+          var key;
+          var options;
 
-        $field.val(value);
+          mutationsList.forEach(function(mutation) {
+            for (var remoteName in remotes) {
+              if (mutation.target === remotes[remoteName].$observable.get(0)) {
+                current = remotes[remoteName];
+              }
+            }
+          })
+
+          if (current.fieldType === 'join') {
+            key = current.$observable.find('[data-chooser-choice]:first').attr('data-chooser-choice');  
+          } else {
+            key = current.$observable.val();
+          }
+
+          if (!key) {
+            clearChoices();
+            return populateChoices([])
+          }
+
+          options = {
+            key: key,
+            ...current
+          };
+
+          delete options.$observable;
+          self.api('get-choices', options, function (data) {
+            field.choices = data;
+            clearChoices();
+            populateChoices(data);
+            populateValue()
+          }, function (err) {
+            if (err) {
+              console.log(err);
+            }
+          });
+        }
+
+        function populateChoices(choices) {
+          _.each(choices, function (choice) {
+            var $option = $('<option></option>');
+            $option.text(choice.label);
+            $option.attr('value', choice.value);
+            $field.append($option);
+          });
+        }
+
+        function clearChoices() {
+          $field.html('');
+        }
+
+        function populateValue() {
+          if (field.choices) {
+            value = ((data[name] === undefined) && field.choices[0]) ? field.choices[0].value : data[name];  
+          } else {
+            value = 'Nothing set';
+          }
+
+          $field.val(value);
+        }
 
         return setImmediate(callback);
       },
